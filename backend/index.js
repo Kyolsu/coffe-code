9c4986bf4e317caaa8dffd17ba5b30122b0827e7
unknown <asosa23@alumnos.uaq.mx>
2026-03-20 09:39:35 -0600

    Rutas de clientes, beneficios y actualizacion de perfil C:

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const db = require('./db'); // Aquí mandamos llamar al archivo de arriba
+const db = require('./db'); 
 const app = express();
 const port = process.env.PORT || 3000;
-
+const usuariosRutas=require('./rutas/usuarios')
+const clientesRutas=require('./rutas/clientes')
 app.use(express.json());
-
-// Endpoint de prueba para ver si la base de datos responde
+//RUTAS DE ENDPOINTS
+app.use('/api/usuarios', usuariosRutas);
+app.use('/api/clientes',clientesRutas);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
