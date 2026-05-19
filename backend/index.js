3df1bad506c801fe8daf39fc56bf9053733719e4
Kyolsu <roberto.calderonp04@gmail.com>
2026-05-19 10:39:07 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const db = require('./db'); 
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 3000;
 const usuariosRutas=require('./rutas/usuarios')
 const clientesRutas=require('./rutas/clientes')
